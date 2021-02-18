4c28eff3d4252720c65bbaaf07366021bdb618c0
kanishkaverma <kanishkaverma@users.noreply.github.com>
2021-02-19 05:14:16 +0530

    reverse linked list

---
+++ reverse_linked_list.py
@@ -0,0 +1,41 @@
+class Node: 
+    def __init__(self, value, next=None): 
+        self.value = value
+        self.next = next 
+
+    def print_list(self): 
+        temp = self 
+        while temp is not None: 
+            print(temp.value, end="->")
+            temp= temp.next 
+        print()
+
+
+def reverse(head): 
+    prev = None 
+    curr,nxt = head, head.next 
+    while(nxt is not None): 
+        curr.next = prev 
+        prev= curr 
+        curr = nxt
+        nxt = nxt.next
+    curr.next = prev 
+
+
+    return curr
+
+
+def main(): 
+    head = Node(2)
+    head.next = Node(4)
+    head.next.next = Node(6)
+    head.next.next.next = Node(8)
+    head.next.next.next.next = Node(10)
+
+    head.print_list()
+
+    result =  reverse(head)
+
+    result.print_list()
+
+main()
